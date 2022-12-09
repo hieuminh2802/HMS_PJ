df7408b26f3196e7a63197eaad64c953704a9608
Corize Hoang Nguyen <corizehoangnguyen@Corizes-MacBook-Air.local>
2022-12-09 20:20:23 +0700

    refactor

--- src/Context/AppContext.js
+++ src/Context/AppContext.js
@@ -1,24 +1,35 @@
-import { useContext } from "react";
+import { useContext, useEffect } from "react";
 import { createContext, useState } from "react";
+import axios from "axios";
 
+export const AppContext = createContext();
 
+export const AppProvider = ({ children }) => {
+  const [user, setUser] = useState(undefined);
+  const [isAuthenticated, setIsAuthenticated] = useState(false);
+  const [cartCount, setCartCount] = useState(0);
 
+  useEffect(() => {
+    axios.get(`/api/getUser`).then((res) => {
+      if (res.data.status === 200) {
+        setIsAuthenticated(true);
+        setUser(res.data.user[0]);
+      }
+    });
+  }, []);
 
-export const AppContext = createContext()
-
-
-export const AppProvider = ({children})=>{
-    const [cartCount, setCartCount] = useState(0)
-
-
-    return <AppContext.Provider
-    value={{
-        cartCount, setCartCount
-    }}
+  return (
+    <AppContext.Provider
+      value={{
+        user,
+        isAuthenticated,
+        cartCount,
+        setCartCount,
+      }}
     >
-        {children}
+      {children}
     </AppContext.Provider>
-}
+  );
+};
 
-
-export const useAppContext = ()=>useContext(AppContext)
+export const useAppContext = () => useContext(AppContext);
